47e8b5b180816c165c2b8f260e501d5d176b068f
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2017-08-21 22:25:03 +0800

    fixed readme

---
+++ src/layouts/index.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import styled, { injectGlobal } from 'styled-components'
+
+//import './index.css'
+
+injectGlobal`
+  html {
+    box-sizing:border-box;
+    font-size:100%;
+  }
+
+  *, *::before, *::after {
+    box-sizing:inherit;
+  }
+
+  body {
+    color:#555;
+    padding-top: 5em;
+    font-family:georgia,serif;
+  }
+
+  h1,h2,
+  h3,h4,
+  h5,h6 {
+    color:#333;
+    font-weight:300;
+  }
+
+`
+
+const SideBarStyles = styled.aside`
+  > div {
+    background-color: #eee;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  height: 100vh;
+`
+
+const HeaderStyles = styled.header`
+  height: 10vh;
+  display: flex;
+  font-weight: 400;
+  justify-content: center;
+  font-family: georgia;
+  font-style: italic;
+  align-items: center;
+  margin-bottom: 3.5em;
+  padding: 0.7em 0 0 0.2em;
+  max-width: 73%;
+  margin-left: auto;
+  margin-right: auto;
+  @media screen and (max-width: 780px) {
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+    max-width: 100%;
+  }
+`
+
+const HeaderLink = styled(Link)`
+  text-decoration:none;
+  font-size: 2.3em;
+  letter-spacing: -0.01em;
+  margin-right:auto;
+  align-self:center;
+  font-weight:500;
+  &:link{
+    color:#555;
+  }
+
+  &:visited {
+    color:#333;
+  }
+  @media screen and (max-width: 780px) {
+    margin-right:0;
+    margin-bottom:10px;
+  }
+
+`
+
+const NavLink = styled(Link)`
+  margin-right: 1.4em;
+  font-size:1.3em;
+  letter-spacing:0.05em;
+  font-variant:small-caps;
+  align-self:center;
+  text-decoration:none;
+  border-bottom:1px solid transparent;
+  will-change:all;
+  transition:border-bottom-color 0.2s linear;
+
+  @media screen and (max-width: 780px) {
+    margin-right:0;
+
+    &:last-child {
+      margin-bottom: 50px;
+    }
+  }
+  &:link {
+    color:#555;
+  }
+
+  &:visited {
+    color:#333;
+  }
+
+  &:hover {
+    border-bottom-color: #222;
+  }
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 65%;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+
+  @media screen and (max-width: 780px) {
+    max-width: 80%;
+  }
+`
+
+const SideBar = () =>
+  <SideBarStyles>
+    <div>asdasd</div>
+    <div />
+    <div />
+  </SideBarStyles>
+
+const Header = () =>
+  <HeaderStyles>
+    <HeaderLink to="/">The Blog</HeaderLink>
+    <NavLink to="/">One</NavLink>
+    <NavLink to="/page-2/">Two</NavLink>
+    <NavLink to="/page-3/">Three</NavLink>
+    <NavLink to="/page-4/">Four</NavLink>
+  </HeaderStyles>
+
+const TemplateWrapper = ({ children }) =>
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]}
+    />
+    <Header />
+    <Container>
+      {children()}
+    </Container>
+  </div>
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+}
+
+export default TemplateWrapper
---
+++ src/pages/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Paragraph = styled.p`
+  max-width: 75%;
+  line-height: 1.47;
+  font-size: 1.2em;
+  text-indent: 1.43em;
+  @media all and (max-width: 800px) {
+    max-width: 95%;
+  }
+
+  @media all and (max-width: 500px) {
+    max-width: 100%;
+  }
+`
+
+const Heading = styled.h1`
+  font-size: 1.89em;
+  font-weight: 500;
+`
+
+const IndexPage = () =>
+  <div>
+    <Heading>Hello to you!</Heading>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id maiores eius
+      rerum unde rem animi distinctio voluptatem tenetur, accusantium, incidunt
+      beatae, ex deserunt architecto autem. Blanditiis possimus impedit
+      accusamus non. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eos tempore deserunt culpa ad facilis voluptate placeat beatae nostrum,
+      nemo necessitatibus accusantium, ex cumque repellat neque officiis fugit,
+      esse ipsa ab. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Itaque eius labore aperiam eligendi nam tempora repudiandae pariatur
+      repellendus eveniet illum, libero aliquam placeat ducimus qui cumque sint
+      molestiae odit quod! Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Officia deserunt repudiandae quod quibusdam, nam odit consequatur
+      est quidem voluptas libero! Aspernatur tempore et nulla libero!
+      Exercitationem error praesentium numquam iste. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Explicabo cum voluptatibus temporibus iure,
+      voluptatem iusto, accusantium natus dolores neque, omnis sequi dolor
+      labore. Suscipit non earum ea impedit libero eum. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Sed inventore odio optio nemo
+      dignissimos veritatis, labore dolores explicabo, dolore tenetur laudantium
+      rem quisquam neque sapiente iste quaerat doloremque consectetur? Facilis.
+    </Paragraph>
+  </div>
+export default IndexPage
